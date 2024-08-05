754bab390aa4e9fec6397a781815fb09f44672ef
Rocky99090 <d369909121@gmail.com>
2024-08-05 11:54:22 +0530

    update

--- src/components/Utilities/Services/Services.js
+++ src/components/Utilities/Services/Services.js
@@ -31,6 +31,7 @@ const Services = () => {
             <div className="s-about-content " data-wow-delay=".4s">
             <p className='text-center'><b>WELCOME TO ROCKY BOOK - THE ULTIMATE DESTINATION FOR ONLINE GAMBLING SITES.
             WE OFFER A WIDE RANGE OF OPTIONS TO BET ON INCLUDING SPORTSBOOKS, LIVE CARDS & LIVE CASINO </b></p>
+            <p>We offer a wide range of options to bet on, including sportsbooks, live cards, and live casino games. Our platform is designed to provide you with an exhilarating and seamless betting experience. Whether you're a seasoned bettor or a newcomer, you'll find an array of opportunities to test your skills and luck. From popular sports events and live card games to the thrill of live casinos, we have something for everyone. Enjoy instant deposits and withdrawals, 24/7 customer support, and a user-friendly interface that makes cricket betting sites easy and fun. Join us today and experience the excitement of online gambling at its best, only at Rocky Book!</p>
 </div>
                 <div className="row">
                     {
